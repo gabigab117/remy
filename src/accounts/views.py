5f8180a682eaea27a43d2cf830bfc10bf9040404
gabigab117 <gabrieltrouve5@outlook.com>
2023-05-20 07:08:52 +0200

    del messages

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,4 +1,3 @@
-from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.views import PasswordChangeView, PasswordResetView, PasswordResetDoneView, \
     PasswordResetConfirmView, PasswordResetCompleteView
