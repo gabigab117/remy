5905d17bcad6406170fb0707c6f4bd8b6a41888e
gabigab117 <gabrieltrouve5@outlook.com>
2023-05-20 17:28:13 +0200

    admin_custom_idea

--- src/ideas/admin.py
+++ src/ideas/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import Category, Idea, RequestIdea
 
 admin.site.register(Category)
-admin.site.register(Idea)
+# admin.site.register(Idea)
 admin.site.register(RequestIdea)
+
+
+@admin.register(Idea)
+class IdeaAdmin(admin.ModelAdmin):
+    list_display = ["name", "summary", "level", "category", "thinker", "status"]
+    list_editable = ["status"]
+    list_display_links = ["name"]
+    search_fields = ["name"]
+    list_filter = ["status"]
