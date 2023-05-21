7aeefe52d09a73b3d2e2c5bc31461eb0fa0e6e33
gabigab117 <gabrieltrouve5@outlook.com>
2023-05-21 21:40:11 +0200

    first_views_index

--- src/ideas/views.py
+++ src/ideas/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Idea, RequestIdea
 
-# Create your views here.
+
+def index(request):
+    ideas: Idea = Idea.objects.filter(status=True)
+    request_ideas: RequestIdea = RequestIdea.objects.filter(status=True)
+
+    return render(request,
+                  template_name="ideas/index.html",
+                  context={"ideas": ideas, "request_ideas": request_ideas})
