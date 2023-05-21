db78792c7c37776345e9a2afe332d49852e592fd
gabigab117 <gabrieltrouve5@outlook.com>
2023-05-21 21:39:57 +0200

    index views

--- src/remy/urls.py
+++ src/remy/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 from . import settings
-from .views import index
+from ideas.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
