17bd9351334bd6f8b75f324277ac684b6afc5077
gabigab117 <gabrieltrouve5@outlook.com>
2023-05-21 21:40:28 +0200

    admin_custom_tuple

--- src/ideas/admin.py
+++ src/ideas/admin.py
@@ -2,14 +2,21 @@ from django.contrib import admin
 from .models import Category, Idea, RequestIdea
 
 admin.site.register(Category)
-# admin.site.register(Idea)
-admin.site.register(RequestIdea)
 
 
 @admin.register(Idea)
 class IdeaAdmin(admin.ModelAdmin):
-    list_display = ["name", "summary", "level", "category", "thinker", "status"]
-    list_editable = ["status"]
-    list_display_links = ["name"]
-    search_fields = ["name"]
-    list_filter = ["status"]
+    list_display = ("name", "summary", "level", "category", "thinker", "status")
+    list_editable = ("status", )
+    list_display_links = ("name", )
+    search_fields = ("name", "details", "summary", )
+    list_filter = ("status", "category", )
+
+
+@admin.register(RequestIdea)
+class RequestIdea(admin.ModelAdmin):
+    list_display = ("name", "summary", "level", "category", "thinker", "status")
+    list_editable = ("status", )
+    list_display_links = ("name", )
+    search_fields = ("name", "details", "summary", )
+    list_filter = ("status", "category", )
