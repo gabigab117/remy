1730330ff91ca585eb463295f5df95f76ccc9875
gabigab117 <gabrieltrouve5@outlook.com>
2023-05-24 00:56:48 +0200

    boostrap_template_view

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -12,10 +12,10 @@ class ThinkerCreationForm(UserCreationForm):
 class ThinkerProfilForm(forms.ModelForm):
     # disabled True va griser le champ. On verra les infos affichées mais rien ne sera envoyé
     email = forms.EmailField(help_text="L'email ne peut pas être modifié ici")
-    password = forms.CharField(widget=forms.PasswordInput())
-    first_name = forms.CharField(disabled=True, max_length=100)
-    last_name = forms.CharField(disabled=True, max_length=100)
-    username = forms.CharField(disabled=True, max_length=100)
+    password = forms.CharField(widget=forms.PasswordInput(), label="Mot de passe")
+    first_name = forms.CharField(disabled=True, max_length=100, label="Prénom")
+    last_name = forms.CharField(disabled=True, max_length=100, label="Nom")
+    username = forms.CharField(disabled=True, max_length=100, label="Pseudo")
 
     class Meta:
         model = get_user_model()
