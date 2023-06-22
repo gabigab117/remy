80e9b31304987265819a1dddaf10dcf5fa496fd2
gabigab117 <gabrieltrouve5@outlook.com>
2023-06-23 01:09:43 +0200

    shipping address admin

--- src/accounts/admin.py
+++ src/accounts/admin.py
@@ -3,4 +3,10 @@ from .models import Thinker, Moderator, ShippingAddresse
 
 admin.site.register(Thinker)
 admin.site.register(Moderator)
-admin.site.register(ShippingAddresse)
+
+
+@admin.register(ShippingAddresse)
+class ShippingAddresseAdmin(admin.ModelAdmin):
+    list_display = ("thinker", "name", "city")
+    list_filter = ("thinker", )
+    search_fields = ("thinker__email", "name", "city")
