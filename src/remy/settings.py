c6301b93302212941346911814f27c029e5effcc
gabigab117 <gabrieltrouve5@outlook.com>
2023-06-29 23:40:53 +0200

    db

--- src/remy/settings.py
+++ src/remy/settings.py
@@ -87,8 +87,15 @@ WSGI_APPLICATION = 'remy.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 if os.getenv("ENV_TYPE") == "PROD":
-    # Connecter à MYSQL
-    pass
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_USER_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+        }
+    }
 else:
     DATABASES = {
         'default': {
